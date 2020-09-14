747bbd7eeac94634e553bfabfd6cd17e6adcc828
liuscoding <liushuai1@ysstech.com>
2020-09-14 18:01:14 +0800

    前端代码

---
+++ es6/js/main.js
@@ -0,0 +1,7 @@
+import abc from './hello.js'
+import {name,add} from './user.js'
+
+
+abc.sum(1,2)
+console.log(name)
+add
---
+++ vue-demo/vue-demo/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>你好 ，Hello ，{{name}}</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            name: "张三"
+        }
+    }
+}
+</script>
+
+<style >
+    
+</style>
